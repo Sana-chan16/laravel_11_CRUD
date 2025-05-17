aea0cb5ca749bc48ba6ff40f52c21da226a63f99
Sana-chan16 <168500611+Sana-chan16@users.noreply.github.com>
2025-05-17 21:39:45 +0800

    Additional Restriction

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 📌 You can add global middleware here:
+        // $middleware->append(\App\Http\Middleware\SomeGlobalMiddleware::class);
+
+        // 📌 Route middleware aliases (like 'auth', 'guest', etc.)
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
